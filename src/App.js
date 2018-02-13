c4b705e2e79264872fb51bc9dc6f029917cfba56
evelinasundin <evelinasundin@hotmail.se>
2018-02-13 16:03:28 +0100

    fp styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import queryString from "query-string";
 import { Router, Route } from "react-router";
 import heropic from "./img/fp-hero.jpeg";
 import black_logo from "./img/highnote_black.png";
+import stats_pic from "./img/stats_pic.jpeg";
+import playlists_pic from "./img/playlists_pic.jpeg";
+import kanye from "./img/kanye.jpeg";
+import casette from "./img/casette.jpeg";
 
 class App extends Component {
   // state = {
@@ -48,7 +52,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div class="logo">
+        <div className="logo">
           <img src={black_logo} />
         </div>
         <div>
@@ -57,17 +61,61 @@ class App extends Component {
           </div>
           <div className="hero-text">
             <h1>high note</h1>
-              <h2>ur track keeper</h2>
+            <h2>ur track keeper</h2>
           </div>
           <div className="button">
-          <a href onClick={() => window.location = 'http://localhost:8888/login' }> login with spotify </a>
+            <a
+              href
+              onClick={() => (window.location = "http://localhost:8888/login")}
+            >
+              {" "}
+              connect with spotify{" "}
+            </a>
           </div>
           <div className="fp-text">
-          <h3> it keeps track of all of your tracks</h3>
-          <p>lorem ipsum lorem ipsum lorem ipsum loooorem ipsum </p>
+            <h3> it keeps track of all of your tracks</h3>
+            <p>lorem ipsum lorem ipsum lorem ipsum loooorem ipsum </p>
           </div>
-        </div> {/* last div */} 
+          <div className ="img-container">
+            <div className="img-holder-left opacity">
+              <img src={stats_pic} className="greyscale" />
+              <h4>stats</h4>
+            </div>
+              <div className="img-holder-right">
+              <img src={playlists_pic} />
+              <h4>playlists</h4>
+            </div>
+            </div>
+          <div className="fp-text">
+            <h3>check your stats and create your own playlists</h3>
+            <p>lorem ipsum lorem ipsum lorem ipsum loooorem ipsum </p>
+          </div>
+          <div className ="img-container">
+          <div className="img-holder-left">
+              <img src={casette} />
+              <h4 className="red">songs</h4>
+            </div>
+              <div className="img-holder-right">
+              <img src={kanye} />
+              <h4>artists</h4>
+            </div>
+            </div>
+            <div className="fp-text">
+            <h3> it keeps track of all of your tracks</h3>
+            <p>lorem ipsum lorem ipsum lorem ipsum loooorem ipsum </p>
+          </div>
+          <div className="hejhej">
+            <a
+              href
+              onClick={() => (window.location = "http://localhost:8888/login")}
+            >
+              {" "}
+              connect with spotify{" "}
+            </a>
+          </div>
 
+
+        </div> {/* last div */}
         {/* <div className="container">
           <p> hejhej </p>
         </div> */}
