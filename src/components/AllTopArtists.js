65a382460bd1663f27204850531dfe9dc775ae1f
evelinasundin <evelinasundin@hotmail.se>
2018-03-08 15:30:28 +0100

    all top artists added

--- src/components/AllTopArtists.js
+++ src/components/AllTopArtists.js
@@ -1,63 +1,15 @@
-// import React, { Component } from "react";
-// import "../App.css";
-// import queryString from "query-string";
-
-// class AllTopArtists extends Component {
-
-//   componentDidMount() {
-//     // extracts the querystring from url returns object
-//     let parsedURL = queryString.parse(window.location.search);
-//     let accessToken = parsedURL.access_token;
-
-//       fetch("https://api.spotify.com/v1/me/top/artists?time_range=long_term&&limit=50", {
-//         headers: { Authorization: "Bearer " + accessToken }
-//         // which returns a response as a promise
-//       })
-//         .then(response => response.json())
-//         .then(data =>
-//           this.setState({
-//             topArtists: data
-//           })
-//         );
-
-//     fetch(
-//       "https://api.spotify.com/v1/me/top/tracks?time_range=long_term&&limit=50",
-//       {
-//         headers: { Authorization: "Bearer " + accessToken }
-//         // which returns a response as a promise
-//       }
-//     )
-//       .then(response => response.json())
-//       .then(data => console.log(data)
-//         // this.setState({
-//         //   topSongs: data
-//         // })
-//       );
-//   }
-
-//   render(
-//   )
-//   {
-//     return (
-//       <div>
-//           <h1> hej </h1>
-//       </div>
-//     );
-//   }
-// }
-
-// export default AllTopArtists;
-
 import React, { Component } from "react";
 import "../App.css";
 import queryString from "query-string";
 import ReactDOM from "react-dom";
 import Modal from "react-responsive-modal";
+import white_logo from "../img/highnote_white.png";
+import PlaylistRenderer from "./PlaylistRenderer";
 
 let parsedURL = queryString.parse(window.location.search);
 let accessToken = parsedURL.access_token;
 
-class AllTopArtists extends Component {
+class AllTopSongs extends Component {
   state = {
     timeRange: "long_term",
     topSongs: {},
@@ -65,6 +17,9 @@ class AllTopArtists extends Component {
   };
 
   componentDidMount() {
+    // to start from beginning of page
+    window.scrollTo(0, 0);
+
     // extracts the querystring from url returns object
     let parsedURL = queryString.parse(window.location.search);
     let accessToken = parsedURL.access_token;
@@ -93,7 +48,7 @@ class AllTopArtists extends Component {
 
   getSongs = timeRange => {
     fetch(
-      `https://api.spotify.com/v1/me/top/tracks?time_range=${timeRange}&&limit=50`,
+      `https://api.spotify.com/v1/me/top/artists?time_range=${timeRange}&&limit=50`,
       {
         headers: { Authorization: "Bearer " + accessToken }
         // which returns a response as a promise
@@ -112,65 +67,63 @@ class AllTopArtists extends Component {
       this.state.topSongs.items &&
       this.state.topSongs.items.map(function(item, i) {
         return (
-          <ul key={i}>
-            <li className="allsongs-li">{i + 1 + "."}</li>
-            <li className="allsongs-li">
-              <div className="alltop-img-cover-container">
+          <ul key={i} className="alltop-ul animate-top">
+            <div className="allsongs-li-container">
+              <li className="allsongs-li">{i + 1 + "."}</li>
+              <li className="allsongs-li">
+                <div className="alltop-img-cover-container">
                 <img
-                  src={item.album.images[1].url}
-                  alt="album cover"
-                  className="alltop-img-cover"
-                />
-              </div>{" "}
-            </li>
+                src={item.images[1].url}
+                alt="album cover"
+                className="artist-img"
+              />
+                </div>{" "}
+              </li>
 
-            <li className="allsongs-li">{item.name}</li>
-            {item.artists.map(function(artist, i) {
-              return (
-                <li
-                  className="inline-block allsongs-li allsongs-artist"
-                  key={artist.id}
-                >
-                  {artist.name}
-                  {i !== item.artists.length - 1 ? ",\xa0" : ""}
-                </li>
-              );
-            })}
+              <li className="allsongs-li">{item.name}</li>
 
-            <li key={item.id} className="allsongs-li allsongs-album">
-              {item.album.name}
-            </li>
-
-            <li className="allsongs-li">
-              <a target="_blank" href={item.external_urls.spotify}>
-                <i className="fa fa-play-circle-o" />
-              </a>
-            </li>
+              <li className="allsongs-li">
+                <a target="_blank" href={item.external_urls.spotify}>
+                  <i className="fa fa-play-circle-o play-btn" />
+                </a>
+              </li>
+            </div>
           </ul>
         );
       });
 
     return (
-      <div>
-        <h1> hej </h1>
-        <form className="submit-playlist" onSubmit={this.getSongs}>
-          <select
-            name="timeRange"
-            placeholder="Select your period of time"
-            className="inline-block"
-            value={this.state.timeRange}
-            onChange={this.onChange}
-            required
-          >
-            <option value="short_term">Last month</option>
-            <option value="medium_term">Half year</option>
-            <option value="long_term">All time</option>
-          </select>
-        </form>
-        {songName}
+      <div className="alltopsongs-container">
+        <div className="alltopsongs-list-container">
+          <div className="logo recentsongs">
+            <img src={white_logo} alt="logo-white" className="inline-block" />
+            <p className="user inline-block white-text">
+              {" "}
+              {this.state.user.name
+                ? this.state.user.name
+                : this.state.user.userID}
+            </p>
+            <i className="fa fa-user" />
+          </div>
+          <div className="timerange-select-container">
+            <select
+              name="timeRange"
+              placeholder="Select your period of time"
+              className="inline-block"
+              value={this.state.timeRange}
+              onChange={this.onChange}
+              required
+            >
+              <option value="short_term">Last month</option>
+              <option value="medium_term">Half year</option>
+              <option value="long_term">All time</option>
+            </select>
+          </div>
+          {songName}
+        </div>
       </div>
     );
   }
 }
 
-export default AllTopArtists;
+export default AllTopSongs;
